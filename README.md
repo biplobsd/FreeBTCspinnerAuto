86a4bc11a10252ad9a541285a501e92a1bccf0db
Biplob SD <biplobsd11@gmail.com>
2019-02-13 16:36:55 +0600

    Added new features

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # FreeBTCspinnerAuto
-<img src="https://img.shields.io/badge/license-MIT-green.svg"> <img src="https://img.shields.io/badge/release-v1.1-red.svg"> 
+<img src="https://img.shields.io/badge/license-MIT-green.svg"> <img src="https://img.shields.io/badge/release-v1.2-red.svg"> 
 
 Now you can think-less touching spin button. This is script done your work.
 It is an automation script using Ankulua like Pokemon GO Plus.
+
+[![Free BTC spinner auto clicker ankulua script](https://raw.githubusercontent.com/biplobsd/FreeBTCspinnerAuto/master/Image/preview_youtube.png)](http://www.youtube.com/watch?v=735N3UM0Y5w "Free BTC spinner auto clicker ankulua script")
 
 ### Environment
 - OS: Android
@@ -12,28 +14,25 @@ It is an automation script using Ankulua like Pokemon GO Plus.
 English: http://ankulua.boards.net/; 
 Chinese: http://ankulua-tw.boards.net/
 - Target app: <a href="https://play.google.com/store/apps/details?id=me.coinspinner.app&hl=en" target="_blank"><span style="color: blue">https://play.google.com/store/apps/details?id=me.coinspinner.app&hl=en</span></a>
-- `(Recommended) use moded adds free version`
+- `(Recommended) use modded adds free version`
 
 
 ### Features
 - [x] Wait for app home page.
 - [x] While Low energy detected then stop.
 - [x] Setting Timer on 3.4m then stop.
-- [x] Checking non-US location popup then stop
+- [x] Checking non-US location popup then stop.
+- [x] Auto Claim Now after low energy detected. 
+- [x] Only Claim now
 
 ### How to Install and Run
-[1] Download it. And save your phone storage.
+ [1] Download and extract it, and save your phone storage.
 
-[2] Download and install Ankulua app. It is on playstore.
+ [2] Download and install Ankulua app. It is on playstore.
 
-[3] Open ankulua and selete the script and start service.
+ [3] Open ankulua and selete the script and start service.
 
-[4] Now Navigate to the Free btc spinner app and play script.
-
-### CONFIGURATION:
-`offer = 1` 0 for check offer popup message. Set 1 for not check offer popup.
-
-`lerrorC = 0` 0 for check Location error popup message. Set 1 fro not check Location error popup.
+ [4] Now Navigate to the `Free btc spinner` app and play the script.
 
 ### Disclaimer:
 - This script may be officially detected and the app account will be dismissed in violation of the Terms of Service.
@@ -42,6 +41,6 @@ Chinese: http://ankulua-tw.boards.net/
 
 ### Tested Device: 
 - Xiaomi redmi 4 prime
+- Xiaomi redmi note 4x
 
-#### Coded By BiplobSD a.k.a alpha4D
-version = "1.0"
+###### Coded By BiplobSD a.k.a alpha4D
--- spin.lua
+++ spin.lua
@@ -8,7 +8,7 @@
 
 coded by biplobsd
 --]]
-version = "\n\nv1.1"
+version = "\n\nv1.2"
 -- ========== Settings ================
 
 -- Globle Variables Setup 
@@ -22,14 +22,37 @@ LCheck = Region(228, 93, 207, 87)
 offer1 = Region(137, 495, 45, 57)
 spin = Region(762, 1709, 62, 22)
 lerrorR = Region(595, 845, 183, 75)
+climeS = Region(286, 807, 284, 64)
+climeC = Region(131, 755, 171, 71)
+climeSuc = Region(297, 793, 259, 57)
+PleWait = Region(163, 763, 757, 99)
+verifybtcapp = Region(896, 69, 128,132)
 
 -- environment setup
 dir = scriptPath();
 setImagePath(dir .. "image")
 
+-- User Interface
+dialogInit()
+newRow()
+addCheckBox("ClimeNow", " Set Auto 'Claim Now' after low energy", false)
+newRow()
+addCheckBox("ClimeNowOnly", " Set only Claim Now.", false)
+newRow()
+addCheckBox("offer", " Set Offer Check", false)
+newRow()
+addCheckBox("lerrorC", " Set Location error Check", false)
+newRow()
+addTextView("Set time eatch app for spin in minutes ")
+addEditNumber("timerSET", 3.42)
+dialogShow("Setup")
+
+timerSET = timerSET * 60
+
 -- Veriable
-offer = 1 -- 0 for check offer popup message. set 1 for not check offer popup.
-lerrorC = 0 -- 0 for check Location error popup message. set 1 fro not check Location error popup.
+-- offer = true -- `true` for check offer popup message. Set `false` for not check offer popup.
+-- lerrorC = true -- `true` for check Location error popup message. Set `false` for not check Location error popup.
+-- ClimeNow = true -- `true` for check clime now. Set `false` for not check.
 timer = Timer();
 -- ========== Function ================
 
@@ -43,8 +66,8 @@ while waitf:exists(Pattern("Wel_W.png"), 0) do
     wait(3)
     toast("Wait")
     toast(timer:check() .. " sec")
-    if timer:check() >= 205.361 then
-        toast("You have maybe internet problem. So your app need more time for load app. \nRuntime: " .. timer:check()/60 .. " minutes" .. version)
+    if timer:check() >= timerSET then
+        scriptExit("You have maybe internet problem. So your app need more time for load app. \nRuntime: " .. timer:check()/60 .. " minutes" .. version)
         break
     end
 end
@@ -52,18 +75,22 @@ end
     -- Spinning
 -----------------
 while not LCheck:exists(Pattern("Low_W.png"), 0.4) do
-    spinsave = spin:exists(Pattern("spin.png"), 0)
-    if spinsave ~= nil then
-        click(Location(752, 1704))
-        click(Location(752, 1704))
-    else
-        toast("Spin not Found. Wait")
+    if (ClimeNowOnly) then
         break
     end
-
+    if ClimeNowOnly == false then
+        spinsave = spin:exists(Pattern("spin.png"), 0)
+        if spinsave ~= nil then
+            click(Location(752, 1704))
+            click(Location(752, 1704))
+        else
+            toast("Spin not Found. Wait")
+            break
+        end
+    end
     -- Offer Check
 -----------------
-    if offer == 0 then
+    if (offer) then
         offersave = offer1:exists(Pattern("of_1.png"), 0)
          if offersave ~= nil then
             toast("Offer found")
@@ -74,7 +101,7 @@ while not LCheck:exists(Pattern("Low_W.png"), 0.4) do
 
     -- Locotion error Check
 -----------------
-    if lerrorC == 0 then
+    if (lerrorC) then
          if lerrorR:exists(Pattern("Lerror.png"), 0) then
             scriptExit("A non-US location detected. So stop. \nRuntime: " .. timer:check()/60 .. " minutes" .. version)
         end
@@ -82,14 +109,51 @@ while not LCheck:exists(Pattern("Low_W.png"), 0.4) do
 
     -- timer end
 -----------------
-    if timer:check() >= 205.361 then
-        scriptExit("Time is over. \nRuntime: " .. timer:check()/60 .. " minutes" .. version)
+    if ClimeNowOnly == false then
+        if timer:check() >= timerSET then
+            toast("Time is over. \nRuntime: " .. timer:check()/60 .. " minutes" .. version)
+            break
+        end
+    end
+end
+
+wait(0.1)
+
+        -- Clime Now
+-----------------
+if (ClimeNow) or (ClimeNowOnly) then
+    if LCheck:exists(Pattern("Low_W.png"), 0.4) or verifybtcapp:exists(Pattern("vRfy.png"), 0) then
+        toast("Try to Claim Satoshi")
+        click(Location(99, 136))
+        click(Location(99, 136))
+        wait(0.3)
+        click(Location(99, 136))
+        if climeS:exists(Pattern("climenowB.png"), 0) then
+            toast("Claim Now found")
+            click(Location(439, 846))
+            wait(0.3)
+            if climeC:exists(Pattern("climenow_Confirm.png"), 0) then
+                click(Location(747, 1143))
+                while PleWait:exists(Pattern("plz_wait.png"), 0) do
+                    wait(0.5)
+                    toast("Wait for payment")
+                end
+                if climeSuc:exists(Pattern("climeSucZ.png"), 0) then
+                    scriptExit("Claim Success! \nRuntime: " .. timer:check()/60 .. " minutes" .. version)
+                end
+            end
+        else
+            scriptExit("Wait for 3 day. Then Claim. \nRuntime: " .. timer:check()/60 .. " minutes" .. version)
+        end
     end
 end
 
     -- Verify condition 
 -----------------
 toast(timer:check())
+if timer:check() >= timerSET then
+    scriptExit("Time is over. \nRuntime: " .. timer:check()/60 .. " minutes" .. version)
+end
 if waitf:exists(Pattern("Wel_W.png"), 0) or LCheck:exists(Pattern("Low_W.png"), 0.4) or spin:exists(Pattern("spin.png"), 0) then
     scriptExit("Low energy detected. So stop. \nRuntime: " .. timer:check()/60 .. " minutes" .. version)
 else
